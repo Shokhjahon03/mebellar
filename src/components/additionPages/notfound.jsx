86b83dd3d2da3b4b6a21b48537bbfc4cd2487331
Shohjahon03 <quvvatovshohjahon707@gmail.com>
2023-12-29 10:10:45 +0500

    a

---
+++ src/components/additionPages/notfound.jsx
@@ -0,0 +1,11 @@
+import { Fragment } from 'react'
+import './notfound.css'
+const notfound = () => {
+  return (
+    <Fragment>
+      
+    </Fragment>
+  )
+}
+
+export default notfound
---
+++ src/components/pages/Home.jsx
@@ -0,0 +1,11 @@
+import { Fragment } from 'react'
+import './home.css'
+const Home = () => {
+  return (
+    <Fragment>
+      
+    </Fragment>
+  )
+}
+
+export default Home
